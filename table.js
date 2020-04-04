b062d6908b8e5f639df8669a60d87f811e305c49
OjusWiZard <ojuswimail@gmail.com>
2020-04-04 18:20:52 +0530

    removed extra loop, cleaned up the code

--- table.js
+++ table.js
@@ -4,18 +4,21 @@ let NewRecovered = 0
 let TotalRecovered = 0
 let NewDeaths = 0
 let TotalDeaths = 0
+
 async function getData() {
 
     const response = await fetch('https://api.covid19api.com/summary')
     let data = await response.json()
-    console.log(data.Countries[98].Country)
+    
+	console.log(data.Countries[98].Country)
     console.log(data.Countries[98].TotalConfirmed)
     console.log(data.Countries[98].NewConfirmed)
     console.log(data.Countries[98].NewRecovered)
     console.log(data.Countries[98].TotalRecovered)
     console.log(data.Countries[98].NewDeaths)
     console.log(data.Countries[98].TotalDeaths)
-    for (var i = 1; i <= 227; i++) {
+    
+	for (var i = 1; i <= 227; i++) {
         document.querySelector('#a' + i + '1').textContent = data.Countries[i].Country
         document.querySelector('#a' + i + '2').textContent = data.Countries[i].TotalConfirmed
         document.querySelector('#a' + i + '3').textContent = data.Countries[i].NewConfirmed
@@ -23,16 +26,14 @@ async function getData() {
         document.querySelector('#a' + i + '5').textContent = data.Countries[i].TotalRecovered
         document.querySelector('#a' + i + '6').textContent = data.Countries[i].NewDeaths
         document.querySelector('#a' + i + '7').textContent = data.Countries[i].TotalDeaths
-    }
-    for (var j = 1; j <= 227; j++) {
-        Total += (data.Countries[j].TotalConfirmed)
-        NewConfirmed += data.Countries[j].NewConfirmed
-        NewRecovered += data.Countries[j].NewRecovered
-        TotalRecovered += data.Countries[j].TotalRecovered
-        NewDeaths += data.Countries[j].NewDeaths
-        TotalDeaths += data.Countries[j].TotalDeaths
-    }
-
+		
+        Total 			+= (data.Countries[i].TotalConfirmed)
+        NewConfirmed 	+= data.Countries[i].NewConfirmed
+        NewRecovered 	+= data.Countries[i].NewRecovered
+        TotalRecovered 	+= data.Countries[i].TotalRecovered
+        NewDeaths 		+= data.Countries[i].NewDeaths
+        TotalDeaths 	+= data.Countries[i].TotalDeaths
+	}
 
     document.querySelector('#Country').textContent = 'Total'
     document.querySelector('#TotalCases').textContent = Total
@@ -44,7 +45,6 @@ async function getData() {
     document.querySelector('#TotalCases1').textContent = Total
     document.querySelector('#TotalRecovered1').textContent = TotalRecovered
     document.querySelector('#TotalDeaths1').textContent = TotalDeaths
-
 
 }
 getData()
